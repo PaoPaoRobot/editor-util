fa75e21e7068f82bf0c922a5f65ebdc0a8e1a20f
Yvon Shong <daiwei.song@outlook.com>
2020-11-10 19:13:05 +0800

    Update download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ opener=urllib.request.build_opener()
 opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 urllib.request.install_opener(opener)
 
-for i in range(3958, 9999, 1):
+for i in range(0, 9999, 1):
     name = str(i).zfill(4)
     file_name = name + ".pdf"
     print(file_name)
